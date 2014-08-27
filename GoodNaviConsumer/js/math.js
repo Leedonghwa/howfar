b6ba0445f9671f1f1b17edb231b04fabd37ec68b
struca <netbooks@naver.com>
2014-08-27 23:12:32 +0900

    Math revised
    revice math

--- GoodNaviConsumer/js/math.js
+++ GoodNaviConsumer/js/math.js
@@ -1,4 +1,21 @@
 // 소수점 자리수 지정
+
+var mathModule = (function() {
+	var my = {};
+	
+	my.roundXL = function (n, digits) {
+		if (digits >= 0) { 
+			return parseFloat(n.toFixed(digits)); // 소수부 반올림
+		}
+		digits = Math.pow(10, digits); // 정수부 반올림
+		var t = Math.round(n * digits) / digits;
+
+		return parseFloat(t.toFixed(0));
+	};
+	
+	return my;
+}());
+
 function roundXL(n, digits) {
 	  if (digits >= 0) return parseFloat(n.toFixed(digits)); // 소수부 반올림
 
